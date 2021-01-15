052ed34594032e5c250015f921e174ca8b916979
Guilherme Cesar Leao Da Silva(BASIS) <guilhermeleao@mec.gov.br>
2021-01-15 08:56:17 -0300

    setup inicial

---
+++ jogo.js
@@ -0,0 +1,9 @@
+console.log('[DevSoutinho] Flappy Bird');
+
+const sprites = new Image();
+sprites.src = './sprites.png';
+
+const canvas = document.querySelector('canvas');
+const contexto = canvas.getContext('2d');
+
+
